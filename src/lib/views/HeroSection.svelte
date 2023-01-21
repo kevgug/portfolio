f136d8d7a0eb8d157f402cc5242cbcab283d2e99
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2023-01-20 23:01:05 -0600

    Initial commit

---
+++ src/lib/views/HeroSection.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  const atfPhone = "/images/atf-phone.png";
+</script>
+
+<div class="container row">
+  <img src={atfPhone} />
+  <div>
+    <h2>Kevin Gugelmann</h2>
+    <h1>Digital Designer &<br />Front-End Developer</h1>
+    <ul>
+      <li>
+        Self-driven student at the University of Chicago, studying Computer
+        Science & Business Economics.
+      </li>
+      <li>Designing digital experiences for over 6 years.</li>
+      <li>
+        Conceptualizing and building meaningful software products using native
+        and cross-platform technologies.
+      </li>
+    </ul>
+  </div>
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+  import HeroSection from "../lib/views/HeroSection.svelte";
+</script>
+
+<HeroSection />
