98b9bb7b69a585d1ef6ee003017fd9f84a627b89
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2023-02-07 22:06:12 -0600

    'scroll-to-element' module declaration file

---
+++ src/lib/util/scroll-to-element.d.ts
@@ -0,0 +1,12 @@
+declare module 'scroll-to-element' {
+	interface ScrollToOptions {
+	  ease?: string;
+	  align?: 'top' | 'middle' | 'bottom';
+	  duration?: number;
+	}
+  
+	function scrollTo(elem: HTMLElement | string, options?: ScrollToOptions): void;
+  
+	export default scrollTo;
+  }
+  
