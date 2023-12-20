98e9544c96b2ab3eff109b1285e9a9924f342378
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2023-12-20 01:00:04 +0100

    Updates

--- internal/immutable/chunks/2-5b773e5f.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-53b03aaf.js";export{t as component};
---
+++ internal/immutable/chunks/2-da67aff2.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-c45a3609.js";export{t as component};
