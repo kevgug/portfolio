9081be72dc9a1ff70cb2fa64fc69b23f4c156d4e
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2023-12-22 11:48:14 +0100

    Add Microsoft Clarity tracking script

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -77,6 +77,21 @@
     defer
     src="https://scripts.simpleanalyticscdn.com/latest.js"
   ></script>
+
+  <script type="text/javascript">
+    (function (c, l, a, r, i, t, y) {
+      c[a] =
+        c[a] ||
+        function () {
+          (c[a].q = c[a].q || []).push(arguments);
+        };
+      t = l.createElement(r);
+      t.async = 1;
+      t.src = "https://www.clarity.ms/tag/" + i;
+      y = l.getElementsByTagName(r)[0];
+      y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "k9uwm0mq3h");
+  </script>
   <script>
     (function () {
       function saLoadedLinkEvents() {
