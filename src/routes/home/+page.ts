f19aa24d7e07b51fcda1f78f8e79a6aa4e5aa1fa
Kevin Gugelmann <kevin.gugelmann@outlook.com>
2025-09-08 17:54:08 +0100

    fix "/home" -> "/" redirect

--- src/routes/home/+page.ts
+++ src/routes/home/+page.ts
@@ -1,3 +1,6 @@
 import { redirect } from "@sveltejs/kit";
+import type { PageLoad } from "./$types";
 
-throw redirect(301, "/");
+export const load: PageLoad = () => {
+  throw redirect(301, "/");
+};
